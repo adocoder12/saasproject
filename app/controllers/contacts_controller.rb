15f62075dc6e3ce920ada39e08eded8cf178098f
Adonay D´agosto <adonay.jadal@gmail.com>
2016-12-14 17:26:12 +0000

    modified contact controller submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,25 @@
 class ContactsController < ApplicationController
   def new
-      @contact = Contact.new
+       @contact = Contact.new
   end
   
   def create
+    @contact = Contact.new(contact_params)
+      
+       
+    
+      if @contact.save
+        redirect_to new_contact_path, notice: "message sent."
+      else
+        redirect_to new_contact_path, notice: "error occured."
+      end
   end
+  
+  private
+    def contact_params
+     params.requiere(:contact).permit(:name, :email, :comments)
+    end
+   
 
 end
 
